4bfc4dd892784aab2bf5fc8b27ea0ce4af983e86
OGPhantom <nikita.storchai@nure.ua>
2025-10-12 20:08:49 +0300

    Initial Commit

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Никита Сторчай on 12.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
